26d3d94c8f5ee968f33a86fcbd5afe9ff80124bd
philhannant <philip.a.hannant@gmail.com>
2018-06-16 23:42:20 +0100

    BookingSystemActorTest refactored to use Futures in order to check responses are correct

--- src/test/scala/BookingSystemActorTest.scala
+++ src/test/scala/BookingSystemActorTest.scala
@@ -3,17 +3,30 @@ import java.text.SimpleDateFormat
 import actors.BookingSystemActor
 import akka.actor.{ActorRef, ActorSystem, PoisonPill, Props}
 import messages.{AddBooking, IsRoomAvailable}
+import akka.util.Timeout
+import akka.pattern.ask
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scala.concurrent.{Await, ExecutionContext, Future, duration}
 
 object BookingSystemActorTest extends App{
 
   val system = ActorSystem("bookingSystem")
   val bookingSystemActor = system.actorOf(Props[BookingSystemActor], "bookingSystemActor")
   val today = new SimpleDateFormat("yyyy-MM-dd").parse("2018-06-14")
+  implicit val timeout = Timeout(5 seconds)
 
   val bmActorRef: ActorRef = bookingSystemActor
-  bmActorRef ! IsRoomAvailable(101,today) // outputs RoomAvaiable()
-  bmActorRef ! AddBooking("Smith",101,today) // outputs BookingMade()
-  bmActorRef ! AddBooking("Jones",101,today) // outputs ErrorOccurred()
+
+  val isRoomAvailable = bmActorRef ? IsRoomAvailable(101,today)
+  println(Await.result(isRoomAvailable, timeout.duration))
+
+  val addBookingSuccess = bmActorRef ? AddBooking("Smith",101,today)
+  println(Await.result(addBookingSuccess, timeout.duration))
+
+  val addBookingFailure = bmActorRef ? AddBooking("Jones",101,today)
+  println(Await.result(addBookingFailure, timeout.duration))
 
   bmActorRef ! PoisonPill
   system.terminate()
