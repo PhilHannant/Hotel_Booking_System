14d93cd0d4c89f80a87fc209854888359df015a7
philhannant <philip.a.hannant@gmail.com>
2018-06-16 22:09:56 +0100

    changed addbooking to return boolean

--- src/main/scala/system/BookingManager.scala
+++ src/main/scala/system/BookingManager.scala
@@ -2,6 +2,8 @@ package system
 
 import java.util.Date
 
+import scala.util.Try
+
 trait BookingManager {
   /**
     * Return true if there is no booking for the given room on the date,
@@ -12,7 +14,7 @@ trait BookingManager {
     * Add a booking for the given guest in the given room on the given
     * date. If the room is not available, throw a suitable Exception.
     */
-  def addBooking(guest: String, room: Int, date: Date): Unit
+  def addBooking(guest: String, room: Int, date: Date): Boolean
 
   /**
     * Return a list of all the available room numbers for the given date
