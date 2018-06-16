5af9d612d6ad4818cdccd4afff45f834d450e40b
philhannant <philip.a.hannant@gmail.com>
2018-06-16 22:25:29 +0100

    bookingSystemTest added

---
+++ src/test/scala/BookingSystemActorTest.scala
@@ -0,0 +1,19 @@
+import java.text.SimpleDateFormat
+
+import actors.BookingSystemActor
+import akka.actor.{ActorRef, ActorSystem, Props}
+import messages.{AddBooking, IsRoomAvailable}
+
+object BookingSystemActorTest extends App{
+
+  val system = ActorSystem("bookingSystem")
+  val bookingSystemActor = system.actorOf(Props[BookingSystemActor], "bookingSystemActor")
+  val today = new SimpleDateFormat("yyyy-MM-dd").parse("2018-06-14")
+
+  val bmActorRef: ActorRef = bookingSystemActor
+  bmActorRef ! IsRoomAvailable(101,today) // outputs RoomAvaiable()
+  bmActorRef ! AddBooking("Smith",101,today) // outputs BookingMade()
+  bmActorRef ! AddBooking("Jones",101,today) // outputs ErrorOccurred()
+
+}
+
