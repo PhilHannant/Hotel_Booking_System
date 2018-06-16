34abb30053adbf696da58423eecf8c666487d2e3
philhannant <philip.a.hannant@gmail.com>
2018-06-16 22:25:00 +0100

    changed addbooking to return boolean

--- src/main/scala/system/BookingManagerSystem.scala
+++ src/main/scala/system/BookingManagerSystem.scala
@@ -3,8 +3,9 @@ package system
 import java.util.Date
 
 import scala.collection.concurrent
-
 import bookingManagerExceptions.RoomNotAvailableException
+
+import scala.util.{Failure, Success, Try}
 
 class BookingManagerSystem extends BookingManager  {
 
@@ -38,12 +39,14 @@ class BookingManagerSystem extends BookingManager  {
     * Add a booking for the given guest in the given room on the given
     * date. If the room is not available, throw a suitable Exception.
     */
-  override def addBooking(guest: String, room: Int, date: Date): Unit = {
+  override def addBooking(guest: String, room: Int, date: Date): Boolean = {
     if (bookings.isEmpty) {
       bookings.put(guest, Booking(room, date))
+      true
     } else {
       if (isRoomAvailable(room, date)) {
         bookings.put(guest, Booking(room, date))
+        true
       } else {
         throw RoomNotAvailableException(s"Room $room not available")
       }
